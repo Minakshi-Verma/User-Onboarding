305a142999d29a5f14fb2119860f27099a8def5e
Minakshi Verma <mvedwan@gmail.com>
2020-01-30 18:43:43 -0500

    "Setting up the Form"

--- my-app/src/components/UserForm.js
+++ my-app/src/components/UserForm.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {withFormik, Form, Field} from 'formik';
 import *as Yup from 'yup';
 import axios from 'axios';
 
 
-const UserForm = () =>{
+const UserForm = ({values, errors, touched, status}) =>{
 
+    //local state that holds successful form submission data
+    const [users, setUsers] = useState([]);
+
+    //listen for status changes to update animal state
+    useEffect(() =>{
+        status && setUsers(users => [...users,status])
+
+    }, [status]);
 
 
     return(
@@ -18,8 +26,11 @@ const UserForm = () =>{
                   type = "text"
                   name = "name"
                   placeholder = "name" />
-
+                  {touched.name && errors.name && (
+                    <p>{errors.name}</p>
+                  )}
                 </label>
+
                 <label htmlFor = 'email'>
                   Email
                   <Field
@@ -27,8 +38,11 @@ const UserForm = () =>{
                    type = "email"
                    name = "email"
                    placeholder = "email" />
-
+                   {touched.email && errors.email && (
+                    <p>{errors.email}</p>
+                  )}
                 </label>
+
                 <label htmlFor = 'password'>
                   Password
                   <Field
@@ -36,32 +50,49 @@ const UserForm = () =>{
                    type = "password"
                    name = "password"
                    placeholder = "password" />
-
+                   {touched.password && errors.password && (
+                    <p>{errors.password}</p>
+                  )}
                 </label>
+
                 <label htmlFor = 'terms'>
                   Term of Service
                   <Field
                    id = "terms"
                    type = "checkbox"
                    name = "terms"
-                //    placeholder = "" 
-                />
-
-
+                   checked ={values.terms}/>
                 </label>
+
                 <button>Submit</button>
-
-
             </Form>
-
-            {/* Name
-            Email
-            Password
-            Terms of Service (checkbox)
-            A Submit button to send our form data to the server. */}
-
+            {users.map(user =>{
+                return(
+                    <ul key = {user.id}>
+                        <li>Name:{user.name}</li>
+                        <li>Email:{user.email}</li>
+                        <li>Password: {user.password}</li>
+                        <li>terms: {user.terms}</li>
+                    </ul>
+                )
+            })}
         </div>
     )
 }
+
+const FormikUserForm = withFormik({
+    
+ mapPropsToValues(props){
+    //setting up initial state of form to value from parent component OR the initial value (after||) 
+    return{
+        name: props.name || "",
+        email: props.email || "",
+        password: props.password || "",
+        terms: props.terms
+
+    }
+ }
+
+})
 
 export default FormikUserForm;
